af3b86e06a5bd533867f99c70c0761d79c0080a2
Joljolionn <joljolionn@gmail.com>
2026-03-25 10:56:03 -0300

    versao atualizada do pipeline com sonar

---
+++ src/soma.js
@@ -0,0 +1,21 @@
+function soma(a, b) {
+ let resultado = a + b;
+ let x = 10; 
+ return resultado;
+}
+
+function soma(a, b) {
+ return a + b;
+}
+function soma2(a, b) {
+ return a + b;
+}
+
+function soma(a, b) {
+eval("console.log('teste')");
+return a + b;
+}
+
+module.exports = soma;
+
+const pasword = "1234";
